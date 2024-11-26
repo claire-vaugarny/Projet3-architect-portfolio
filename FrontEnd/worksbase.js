c1e0cf9fb1ba7ad6a7759675d5c1621ec2aa2ba9
Claire Vaugarny <claire.vaugarny@laposte.net>
2024-11-26 17:42:00 +0100

    affichage des filtres et affichage des projets par categorie(fonction uniquement)

--- FrontEnd/worksbase.js
+++ FrontEnd/worksbase.js
@@ -5,23 +5,69 @@
 // });
 
 const gallery = document.querySelector('.gallery');
+const menuFilters = document.querySelector('.menuFilters');
 
 //pour ajouter un projet dans gallery
 function affichageProjet(projet){
-    gallery.innerHTML+='<article>' //+= permet d'jouter chaque nouveau projet au lieu de le remplacer
-        + '<img src="'+ projet.imageUrl + '" alt="'+projet.title+'"/>'
-        + '<figcaption>'+ projet.title+ '</figcaption>'
-        +'</article>'
+    const article = document.createElement('article');
+    gallery.appendChild(article);
+    const image = document.createElement('img');
+    image.src = projet.imageUrl;
+    image.alt = projet.title;
+    article.appendChild(image);
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = projet.title;
+    article.appendChild(figcaption);
 }
+
+//pour ajouter un filter dans .menuFilter
+function affichageFiltre(filter){
+    const li = document.createElement('li');
+    li.textContent=filter;
+    menuFilters.appendChild(li)
+}
+
+// affichage des projets en fonction de category donné
+function afficherProjetsFiltres(projets, category) {
+    gallery.innerHTML=""; //efface les projets précédents
+    // Filtrer les projets dont la catégorie correspond à category.name
+    const projetsFiltres = projets.filter(projet => projet.category.name === category);
+
+    // Utiliser la fonction affichageProjet() pour afficher chaque projet filtré
+    projetsFiltres.forEach(projet => {
+        affichageProjet(projet);  // Appelle ta fonction d'affichage pour chaque projet
+    });
+}
+
 
 
 fetch("http://localhost:5678/api/works").then((response)=>{
     return response.json();
 }).then((projets)=>{
-    // console.log(projets);
-    document.querySelector('.gallery').innerHTML="";  //efface les projets préccédents et permet de les mettre à jour
+    console.log(projets);
+    //récupère les filtres, creer un nouveau tableau sans doublons
+    const categories = projets.map(projet=>projet.category.name) //création du tableau avec toutes les catégories
+    const categorieSansDoublons =['Tous']; //ajout de Tous car le filtre n'existe pas dans les catégories dans json
+    categories.forEach(categorie=>{
+        if (!categorieSansDoublons.includes(categorie)){categorieSansDoublons.push(categorie)}
+    }) 
+    console.log(categorieSansDoublons);
+
+    //affichage des filtres récupérés
+    menuFilters.innerHTML=""; //efface les filtres précédents
+    categorieSansDoublons.forEach(filter=>{
+        affichageFiltre(filter); //affiche tous les filtres mis à jour
+    })
+    
+
+    //affichage de tous les projets
+    gallery.innerHTML="";  //efface les projets précédents
     projets.forEach(projet => {
-        affichageProjet(projet);
+        affichageProjet(projet); //affiche tous les projets mis à jour
+
+        afficherProjetsFiltres(projets,"Hotels & restaurants")
+        afficherProjetsFiltres(projets,"Objets")
+        afficherProjetsFiltres(projets,"Appartements")
     });
 });
 
@@ -37,19 +83,13 @@ fetch("http://localhost:5678/api/works").then((response)=>{
 
 
 
-
-
-
-
 //Note à moi meme
-//on peut aussi utiliser ça dans la fonction affichageProjet
-// const article = document.createElement('article');
-// const image = document.createElement('img');
-// image.src = projet.imageUrl;
-// image.alt = projet.title;
-// const figcaption = document.createElement('figcaption');
-// figcaption.textContent = projet.title;
-// article.appendChild(image);
-// article.appendChild(figcaption);
-// gallery.appendChild(article);
+//on peut aussi utiliser ça dans la fonction affichageProjet mais modifier le DOM peut entrainer des problèmes de comportement par la suite
+// gallery.innerHTML+='<article>' //+= permet d'jouter chaque nouveau projet au lieu de le remplacer
+// + '<img src="'+ projet.imageUrl + '" alt="'+projet.title+'"/>'
+// + '<figcaption>'+ projet.title+ '</figcaption>'
+// +'</article>'
+
+
+
 
